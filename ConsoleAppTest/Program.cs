d317010690c74a47556e39ba33b1c7d8f4c4e150
郭旭东 <517371617@qq.com>
2018-03-09 11:16:53 +0800

    随意练习

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using IBatisNet.DataMapper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bbb = new BBB();
+            var c = bbb.MyTest();
+            var d = MyClass.MyTest(bbb);
+        }
+    }
+
+    public interface AAA{
+
+    }
+
+    public class BBB : AAA { 
+    }
+
+    public static class MyClass{
+        public static String MyTest(this AAA aaa) {
+            return aaa.ToString();
+        }
+    } 
+}
---
+++ WinFormTest/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormTest
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = await GetString();
+        }
+
+
+        public async Task<string> GetString()
+        {
+            var a = await Task.Run(()=>{
+                Thread.Sleep(5000);
+                return "abc";
+            });
+
+           return a;
+        }
+    }
+}
